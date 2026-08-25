afcfac7c8f768f5ed681ea267994d07a1de84b4f
FPabloA <pabloayala@protonmail.com>
2026-08-25 12:29:14 -0500

    Implemented EpisodeIdentifier and EpisodeProvider classes

---
+++ FPabloA.Jellyfin.OnePacePlugin/EpisodeIdentifier.cs
@@ -0,0 +1,77 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaBrowser.Controller.Providers;
+using FPabloA.Jellyfin.OnePacePlugin.Model;
+
+namespace FPabloA.Jellyfin.OnePacePlugin
+{
+    internal static class EpisodeIdentifier
+    {
+        public static async Task<IEpisode?> IdentifyAsync(
+            IRepository repository,
+            ItemLookupInfo itemLookupInfo,
+            CancellationToken cancellationToken)
+        {
+            //attempt to retrieve metadata using the episode ID (probably not relevant to this)
+            var episodeId = itemLookupInfo.GetOnePaceId();
+            if (episodeId != null)
+            {
+                var episodeInfo = await repository
+                    .FindEpisodeByIdAsync(episodeId, cancellationToken)
+                    .ConfigureAwait(false);
+                if (episodeInfo != null)
+                {
+                    return episodeInfo;
+                }
+            }
+
+            if (itemLookupInfo.Path != null && IdentifierUtil.OnePaceInvariantTitleRegex.IsMatch(itemLookupInfo.Path))
+            {
+                var episodes = await repository.FindAllEpisodesAsync(cancellationToken).ConfigureAwait(false);
+
+                // All of these file names should get matched properly:
+                // - "[One Pace][3-5] Romance Dawn 03 [1080p][D767799C]" (case 1)
+                // - "Romance Dawn 03" (case 3)
+                // - "3-5" (case 2)
+                var fileName = Path.GetFileNameWithoutExtension(itemLookupInfo.Path);
+
+                //match against CRC-32 (Case 1)
+                foreach (var episode in episodes)
+                {
+                    if (episode.Crc32 != null)
+                    {
+                        var pattern = $@"\b{episode.Crc32.Value:XB}\b";
+                        if (Regex.IsMatch(fileName, pattern, RegexOptions.IgnoreCase))
+                        {
+                            return episode;
+                        }
+                    }
+                }
+
+                //match against chapter ranges (case 2)
+                foreach (var episode in episodes.OrderByDescending(episode => episode.MangaChapters?.Length ?? 0))
+                {
+                    if (!string.IsNullOrEmpty(episode.MangaChapters) && IdentifierUtil.BuildTextRegex(episode.MangaChapters).IsMatch(fileName))
+                    {
+                        return episode;
+                    }
+                }
+
+                //match against invariant titles (case 3)
+                foreach (var episode in episodes.OrderByDescending(episode => episode.InvariantTitle.Length))
+                {
+                    if (!string.IsNullOrEmpty(episode.InvariantTitle) && IdentifierUtil.BuildTextRegex(episode.InvariantTitle).IsMatch(fileName))
+                    {
+                        return episode;
+                    }
+                }
+
+            }
+
+            return null;
+        }
+    }
+}
---
+++ FPabloA.Jellyfin.OnePacePlugin/EpisodeProvider.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using ICU4N.Util;
+using MediaBrowser.Common.Net;
+using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Providers;
+using Microsoft.Extensions.Logging;
+
+//Populates One Pace episode metadata from the onepacerr API
+namespace FPabloA.Jellyfin.OnePacePlugin
+{
+    public class EpisodeProvider : IRemoteMetadataProvider<Episode, EpisodeInfo>, IHasOrder
+    {
+        private readonly IRepository _repository;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<EpisodeProvider> _log;
+
+        public EpisodeProvider(
+            IRepository repository,
+            IHttpClientFactory httpClientFactory,
+            ILogger<EpisodeProvider> logger)
+        {
+            _repository = repository;
+            _httpClientFactory = httpClientFactory;
+            _log = logger;
+        }
+
+        public int Order => -1000;
+
+        public string Name => Plugin.ProviderName;
+
+        public async Task<MetadataResult<Episode>> GetMetadata(EpisodeInfo info, CancellationToken cancellationToken)
+        {
+            var result = new MetadataResult<Episode>();
+
+            var episodeMatch = await EpisodeIdentifier.IdentifyAsync(_repository, info, cancellationToken).ConfigureAwait(false);
+            if (episodeMatch != null)
+            {
+                var arc = await _repository.FindArcByIdAsync(episodeMatch.ArcId, cancellationToken).ConfigureAwait(false);
+                if (arc != null)
+                {
+                    result.HasMetadata = true;
+                    result.Provider = Name;
+
+                    result.Item = new Episode
+                    {
+                        IndexNumber = episodeMatch.Rank,
+                        ParentIndexNumber = arc.Rank,
+                        Name = episodeMatch.InvariantTitle,
+                        //episodes do have release date data
+                        PremiereDate = episodeMatch.ReleaseDate,
+                        ProductionYear = episodeMatch.ReleaseDate?.Year
+                    };
+
+                    result.Item.SetOnePaceId(episodeMatch.Id);
+
+                    var localization = await _repository.FindBestLocalizationByEpisodeIdAsync(
+                        episodeMatch.Id,
+                        info.MetadataLanguage ?? "en",
+                        cancellationToken)
+                        .ConfigureAwait(false);
+                    if (localization != null)
+                    {
+                        result.Item.Name = localization.Title;
+                        result.Item.Overview = localization.Description;
+                    }
+
+                }
+                else
+                {
+                    _log.LogError("Could not find arc {ArcId}", episodeMatch.ArcId);
+                }
+
+            }
+
+            _log.LogInformation(
+                    "Identified Episode {Info} --> {March}",
+                    JsonSerializer.Serialize(info),
+                    JsonSerializer.Serialize(episodeMatch));
+
+            return result;
+        }
+
+        public async Task<IEnumerable<RemoteSearchResult>> GetSearchResults(
+            EpisodeInfo searchInfo,
+            CancellationToken cancellationToken)
+        {
+            var result = new List<RemoteSearchResult>();
+
+            var metadataResult = await GetMetadata(searchInfo, cancellationToken).ConfigureAwait(false);
+            if (metadataResult.HasMetadata)
+            {
+                var episode = metadataResult.Item;
+
+                result.Add(new RemoteSearchResult
+                {
+                    IndexNumber = episode.IndexNumber,
+                    ParentIndexNumber = episode.ParentIndexNumber,
+                    Name = episode.Name,
+                    PremiereDate = episode.PremiereDate,
+                    ProductionYear = episode.ProductionYear,
+                    Overview = episode.Overview,
+                    ProviderIds = episode.ProviderIds,
+                    SearchProviderName = Name
+                });
+            }
+
+            return result;
+        }
+
+        public Task<HttpResponseMessage> GetImageResponse(string url, CancellationToken cancellationToken)
+        {
+            return _httpClientFactory.CreateClient(NamedClient.Default).GetAsync(url, cancellationToken);
+        }
+
+    }
+}
