045398513cf8427204d7b505bcdf7ce95fa76364
FPabloA <pabloayala@protonmail.com>
2026-08-27 13:48:31 -0500

    Implemented SeriesIdentifierTests

---
+++ FPabloA.Jellyfin.OnePacePlugin.Tests/SeriesIdentifierTests.cs
@@ -0,0 +1,80 @@
+﻿using FPabloA.Jellyfin.OnePacePlugin.Model;
+using MediaBrowser.Controller.Providers;
+using Moq;
+using Xunit;
+
+namespace FPabloA.Jellyfin.OnePacePlugin.Tests
+{
+    public class SeriesIdentifierTests
+    {
+        private readonly IRepository _repository;
+
+        private class TestSeries : ISeries
+        {
+            public string InvariantTitle { get; init; } = null!;
+
+            public string OriginalTitle { get; init; } = null!;
+        }
+
+        public SeriesIdentifierTests()
+        {
+            var series = new TestSeries
+            {
+                InvariantTitle = "One Pace",
+                OriginalTitle = "One Piece"
+            };
+
+            var repositoryMock = new Mock<IRepository>(MockBehavior.Strict);
+            repositoryMock
+                .Setup(repository => repository.FindSeriesAsync(It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<ISeries?>(series));
+
+            _repository = repositoryMock.Object;
+        }
+
+        [Fact]
+        public async Task ShouldIdentifySeriesByProviderId()
+        {
+            var itemLookupInfo = new ItemLookupInfo();
+            itemLookupInfo.SetOnePaceId(Plugin.DummySeriesId);
+
+            var series = await SeriesIdentifier.IdentifyAsync(_repository, itemLookupInfo, CancellationToken.None);
+
+            Assert.NotNull(series);
+            Assert.Equal("One Pace", series.InvariantTitle);
+        }
+
+        [Theory]
+        [InlineData("/path/to/One Pace")]
+        [InlineData("/path/to/One Piece [One Pace]")]
+        public async Task ShouldIdentifySeriesByPath(string path)
+        {
+            var itemLookupInfo = new ItemLookupInfo
+            {
+                Path = path
+            };
+
+            var series = await SeriesIdentifier.IdentifyAsync(_repository, itemLookupInfo, CancellationToken.None);
+
+            Assert.NotNull(series);
+            Assert.Equal("One Pace", series.InvariantTitle);
+        }
+
+        [Theory]
+        [InlineData("One Pace")]
+        [InlineData("One Piece [One Pace]")]
+        public async Task ShouldIdentifySeriesByName(string name)
+        {
+            var itemLookupInfo = new ItemLookupInfo
+            {
+                Name = name
+            };
+
+            var series = await SeriesIdentifier.IdentifyAsync(_repository, itemLookupInfo, CancellationToken.None);
+
+            Assert.NotNull(series);
+            Assert.Equal("One Pace", series.InvariantTitle);
+        }
+
+    }
+}
