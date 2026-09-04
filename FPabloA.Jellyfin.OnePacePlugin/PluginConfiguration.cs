6f299a5b13ebdaac86fb53366f10adb0a07734f5
FPabloA <pabloayala@protonmail.com>
2026-09-04 13:07:13 -0500

    Removed commented out code from PluginConfiguration

--- FPabloA.Jellyfin.OnePacePlugin/PluginConfiguration.cs
+++ FPabloA.Jellyfin.OnePacePlugin/PluginConfiguration.cs
@@ -5,10 +5,5 @@ namespace FPabloA.Jellyfin.OnePacePlugin
 {
     public class PluginConfiguration : BasePluginConfiguration
     {
-        //public string ApiKey { get; set; } = string.Empty;
-
-        //public bool EnableFeature { get; set; } = true;
-
-        //public int MaxResults { get; set; } = 10;
     }
 }
