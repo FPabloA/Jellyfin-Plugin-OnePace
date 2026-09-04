bf6410502baa3fc3f25447a23a5249e39ddabdb1
FPabloA <pabloayala@protonmail.com>
2026-09-04 13:06:02 -0500

    Removed commented out code from IRepository

--- FPabloA.Jellyfin.OnePacePlugin/IRepository.cs
+++ FPabloA.Jellyfin.OnePacePlugin/IRepository.cs
@@ -14,19 +14,11 @@ namespace FPabloA.Jellyfin.OnePacePlugin
         //Retrieves the models for all known arcs. Read-Only (not sure how this is used yet)
         Task<IReadOnlyCollection<IArc>> FindAllArcsAsync(CancellationToken cancellationToken);
 
-        //Retrieves the arc model based on the ID (not sure if this is relevant since onepacerr has no id field for arcs)
-        //TODO:Clearing stuff to do with arcID
-        //Task<IArc?> FindArcByIdAsync(string id, CancellationToken cancellationToken);
-
         //Retrieves the arc model based on the arc number
         Task<IArc?> FindArcByNumberAsync(string arcNum, CancellationToken cancellationToken);
 
         //Retrieves the models for all known episodes. Read-Only (not sure how this is used yet)
         Task<IReadOnlyCollection<IEpisode>> FindAllEpisodesAsync(CancellationToken cancellationToken);
-
-        //Retrieves the arc and model based on the ID (not sure if this is relevant since onepacerr has no id field for episodes)
-        //TODO: Clearing stuff to do with EpisodeID
-        //Task<IEpisode?> FindEpisodeByIdAsync(string id, CancellationToken cancellationToken);
 
         //Retrieves the available series logo art (probably not relevant since onepacerr does not provide image info)
         Task<IReadOnlyCollection<IArt>> FindAllLogoArtBySeriesAsync(CancellationToken cancellationToken);
@@ -34,23 +26,7 @@ namespace FPabloA.Jellyfin.OnePacePlugin
         //Retrieves the available series cover art (probably not relevant since onepacerr does not provide image info)
         Task<IReadOnlyCollection<IArt>> FindAllCoverArtBySeriesAsync(CancellationToken cancellationToken);
 
-        //Retrieves the available arc cover art (probably not relevant since onepacerr does not provide image info, or use arc IDs)
-        //TODO:Clearing stuff to do with arcID
-        //Task<IReadOnlyCollection<IArt>> FindAllCoverArtByArcIdAsync(string arcId, CancellationToken cancellationToken);
-
-        //Retrieves the available episode cover art (probably not relevant since onepacerr does not provide image info, or use arc IDs)
-        //TODO: Clearing stuff to do with EpisodeID
-        //Task<IReadOnlyCollection<IArt>> FindAllCoverArtByEpisodeIdAsync(string episodeId, CancellationToken cancellationToken);
-
         //Retrieves the series localization data (might not be relevant since onepacerr appears to only provide description and title info in one language)
         Task<ILocalization?> FindBestLocalizationBySeriesAsync(string languageCode, CancellationToken cancellationToken);
-
-        //Retrieves the arc localization data (might not be relevant since onepacerr appears to only provide description and title info in one language)
-        //TODO:Clearing stuff to do with arcID
-        //Task<ILocalization?> FindBestLocalizationByArcIdAsync(string arcId, string languageCode, CancellationToken cancellationToken);
-
-        //Retrieves the episode localization data (might not be relevant since onepacerr appears to only provide description and title info in one language)
-        //TODO: Clearing stuff to do with EpisodeID
-        //Task<ILocalization?> FindBestLocalizationByEpisodeIdAsync(string episodeId, string languageCode, CancellationToken cancellationToken);
     }
 }
